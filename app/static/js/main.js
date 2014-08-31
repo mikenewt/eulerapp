50dba4def2067d45f40d2488ffe80df0b5cd59f1
mikenewt <mikejnewt@gmail.com>
2014-08-31 20:30:08 +0800

    Started work on Euler #2

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -16,17 +16,28 @@ $(document).ready(function(){
 
 });
 
+// method for Euler problem number one
+// takes user input from index.html form and returns sum as an answer
 function findDivisibleNumbers(input) {
     var sum = 0;
     
-    console.log("Received input: " + input);
+    // console.log("Received input: " + input);
     
     for (var i = 1; i < input; i++) {
         if ((i % 3 == 0) || (i % 5 == 0)) {
             sum += i;
-            console.log(sum);
+            // console.log(sum);
         }
     }
 
     $('#answerP').text(sum);
+}
+
+function doFibonacci(limit) {      // limit sets the upper bound of the Fibonacci sequence
+    
+}
+
+// method for Euler problem number two
+function findEvenFibonacci(input) {
+
 }
