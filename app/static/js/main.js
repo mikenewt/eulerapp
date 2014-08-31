e4bcfbf3730f6a13d952d346e208ba2a29d6c3d1
mikenewt <mikejnewt@gmail.com>
2014-08-31 18:56:53 +0800

    Added JS code for Euler 1 and additional test elements index

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,5 +1,31 @@
+var answer1 = 233168;
+
 $(document).ready(function(){
 	console.log('hello world');
-	
-	
+
+	$('#answer-submit').click(function(event) {
+		event.preventDefault();
+		console.log('Default prevented.');
+
+		var input = $('#answer-input').val();
+		console.log('Input is: ' + input);
+		findDivisibleNumbers(input);
+
+	});
+
 });
+
+function findDivisibleNumbers(input) {
+    var sum = 0;
+    
+    console.log("Received input: " + input);
+    
+    for (var i = 1; i < input; i++) {
+        if ((i % 3 == 0) || (i % 5 == 0)) {
+            sum += i;
+            console.log(sum);
+        }
+    }
+
+    $('#answerP').text(sum);
+}
