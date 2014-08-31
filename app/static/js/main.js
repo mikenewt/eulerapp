a94da3f4ea596c0f72d042f0538b9f016cdbf14d
ChristopherGS <christopher.samiullah@gmail.com>
2014-08-31 18:41:18 +0800

    basic jasmine test setup

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -2,4 +2,26 @@ $(document).ready(function(){
 	console.log('hello world');
 	
 	
+	/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+	Find the sum of all the multiples of 3 or 5 below 1000.*/
+	
+	//find numbers divisible by 3 below 1000
+	
+	function euler(){
+		
+		var divisibleNumbers = [];
+		
+		for(var i=0; i<1000; i++) {
+			//console.log('running');
+			//console.log(i);
+			
+			if((i % 3 === 0) || (i % 5 === 0)) {
+				divisibleNumbers.push(i);
+				console.log(divisibleNumbers);
+			}
+			
+		}
+	}
+	
+	euler();
 });
---
+++ app/static/js/vendor/jasmine/spec/EulerSpec.js
@@ -0,0 +1,19 @@
+/*
+describe("sum", function() {
+  var maxValue;
+  var divisibleBy;
+*/
+
+  
+describe("A suite", function() {
+  it("contains spec with an expectation", function() {
+    expect(true).toBe(true);
+  });
+});
+
+describe("Euler Question 1", function() {
+  it("has a sum", function() {
+	expect(true).toBe(true);
+  });
+});
+
